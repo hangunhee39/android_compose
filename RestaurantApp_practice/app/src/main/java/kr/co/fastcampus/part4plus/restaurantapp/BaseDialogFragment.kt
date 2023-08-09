1f1c6e410d789a77a71745c377ab34def5923341
hangunhee39 <ghee0831@naver.com>
2023-08-10 01:55:03 +0900

    init

--- RestaurantApp_practice/app/src/main/java/kr/co/fastcampus/part4plus/restaurantapp/BaseDialogFragment.kt
+++
@@ -1,9 +0,0 @@
-package kr.co.fastcampus.part4plus.restaurantapp
-
-import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.activityViewModels
-import kr.co.fastcampus.part4plus.restaurantapp.features.common.viewmodel.ThemeViewModel
-
-open class BaseDialogFragment: DialogFragment() {
-    protected val themeViewModel: ThemeViewModel by activityViewModels()
-}
--- RestaurantApp_practice/app/src/main/java/kr/co/fastcampus/part4plus/restaurantapp/features/feed/domain/usecase/IGetFeedCategoryUseCase.kt
+++
@@ -1,8 +0,0 @@
-package kr.co.fastcampus.part4plus.restaurantapp.features.feed.domain.usecase
-
-import kr.co.fastcampus.part4plus.restaurantapp.features.common.entity.CategoryEntity
-import kr.co.fastcampus.part4plus.restaurantapp.features.common.entity.EntityWrapper
-
-interface IGetFeedCategoryUseCase {
-    suspend operator fun invoke(): EntityWrapper<List<CategoryEntity>>
-}
